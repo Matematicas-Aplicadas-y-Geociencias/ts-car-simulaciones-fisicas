1939aeada233e12a698fe3c69e2bb69b366a830e
J.C. Cajas <carlos.cajas@enesmerida.unam.mx>
2026-03-03 17:44:12 -0600

    ejemplo de región paralela con openmp

--- paralelo/holaMundo.f90
+++ paralelo/holaMundo.f90
@@ -1,3 +1,10 @@
 Program holaMundoOpenMP
-
+  !
+  ! Este programa utiliza una regi'on paralela de OpenMP
+  ! para escribir un mensaje en la pantalla
+  !
+  !$omp parallel
+  write(*,*) "Hola Mundo"
+  !$omp end parallel
+  !
 end Program holaMundoOpenMP
