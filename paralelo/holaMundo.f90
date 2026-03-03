8f70cec114937c8cbb4ecc4c74bd55b9a01f4835
J.C. Cajas <carlos.cajas@enesmerida.unam.mx>
2026-03-03 17:53:39 -0600

    Se agregan mensajes fuera de la región paralela del programa de hola Mundo

--- paralelo/holaMundo.f90
+++ paralelo/holaMundo.f90
@@ -3,8 +3,17 @@ Program holaMundoOpenMP
   ! Este programa utiliza una regi'on paralela de OpenMP
   ! para escribir un mensaje en la pantalla
   !
+  ! Escribimos mensaje fuera de la regi'on paralela
+  write(*,*) "Hola Mundo fuera de la regi'on paralela"
+  !
+  ! Abrimos la regi'on paralela
+  !
   !$omp parallel
   write(*,*) "Hola Mundo"
   !$omp end parallel
   !
+  ! Cerramos la regi'on paralela
+  !
+  ! Escribimos mensaje fuera de la regi'on paralela
+  write(*,*) "Hola Mundo fuera de la regi'on paralela 2"
 end Program holaMundoOpenMP
