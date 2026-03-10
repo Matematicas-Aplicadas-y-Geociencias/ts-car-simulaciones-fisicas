6851fc7187c2941dc9a64d0ca2317da49bc443d9
J.C. Cajas <carlos.cajas@enesmerida.unam.mx>
2026-03-10 17:16:43 -0600

    Ejemplo de suma con regiones paralelas usando una variable de reducción con suma

--- paralelo/suma_regiones_paralelas.f90
+++ paralelo/suma_regiones_paralelas.f90
@@ -1,15 +1,37 @@
-Program sumaRegionesParalelas
+Program suma_paralela
   !
-  ! Este programa tiene la misi'on de sumar los primeros N n\'umeros
-  ! naturales usando solamente regiones paralelas, es decir no se
-  ! usar'an bucles
-  !
-  ! Cargamos el m'odulo de funciones de openmp
+  ! Este programa suma los pimeros N n'umeros usando una regi'on paralela
   !
   use omp_lib
   !
-  ! Variables para describir la regi'on paralela
+  implicit none
   !
-  integer :: nthreads, id_thread, suma
+  integer :: suma, id_thread
   !
-end Program sumaRegionesParalelas
+  suma = 0
+  !
+  ! Abrimos una regi\'on paralela donde el identificador de hilo ser'a privado
+  ! y acumularemos los valores para realizar la suma en una variable especial
+  ! de tipo reducci'on
+  !
+  ! Las regiones paralelas admiten muchas clausulas en su definici'on, las
+  ! m'as usuales son:
+  ! private(lista), shared(lista), reduction(+,*,-: lista), default(lista)
+  ! num_threads(n'umero)
+  !
+  !$omp parallel private(id_thread) reduction(+:suma) num_threads(65)
+  !
+  id_thread = omp_get_thread_num()
+  ! write(*, *) "id_thread: ", id_thread
+  suma = suma + id_thread
+  !
+  ! Cerramos la regi\'on la regi'on paralela. En este punto todas la copias
+  ! privadas de variables se destruyen, y aquellas que est'an declaradas en la
+  ! lista de reducci'on se acumulan de acuerdo al operador indicado
+  !
+  !$omp end parallel
+  !
+  write(*, *) suma
+  !
+end Program suma_paralela
+
