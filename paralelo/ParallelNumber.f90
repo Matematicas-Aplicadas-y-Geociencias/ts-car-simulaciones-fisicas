7e412a52be23c7f75af8d37f37dea10c510ecb8f
Emilio Ramirez <emilracon@gmail.com>
2026-03-10 12:03:25 -0600

    Suma en paralelo

---
+++ paralelo/ParallelNumber.f90
@@ -0,0 +1,19 @@
+program ids_a_n
+  use omp_lib
+  implicit none
+  integer :: N, tid, S
+
+  N = 12
+  S = 0
+
+  !$omp parallel private(tid) shared(N,S)
+    tid = omp_get_thread_num()   ! 0..T-1
+
+    if (tid < N) then
+      !$omp atomic
+      S = S + (tid + 1)
+    end if
+  !$omp end parallel
+
+  print *, "Suma =", S
+end program
