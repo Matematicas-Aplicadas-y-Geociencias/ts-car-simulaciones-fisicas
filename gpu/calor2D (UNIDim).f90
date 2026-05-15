d6cae13f73ea2404be05d82f7890f1608b0228fe
Isma_GE <guerreroismael117@ciencias.unam.mx>
2026-05-14 18:37:40 -0600

    Calor 2D con arreglos 1D  para realizar debung

--- gpu/calor2D (UNIDim).f90
+++ gpu/calor2D (UNIDim).f90
@@ -26,15 +26,15 @@ Program Calor2D
   ! Variables del problema fisico
   !
   double precision :: xx(nx), yy(ny)  ! variables de la malla
-  double precision :: tt(nx*ny,2)     ! vector de incognitas, 1 para la iteraci'on actual y 2 para la anterior
+  double precision :: tt(nx*ny), tr(nx*ny), tt_o(xx*ny)    ! vector de incognitas, 1 para la iteraci'on actual y 2 para la anterior
+  double precision :: tt_m(nx,ny) !forma matricial de tt
   double precision :: resid_tt(nx,ny) ! vector de residuo
   double precision :: rr(nx*ny)   ! vector de resultados del s. ecuaciones
-  double precision :: cfx(nx,2), cfy(ny,2)! vector de condiciones de frontera
+  double precision :: cfx(ny,2), cfy(nx,2)! vector de condiciones de frontera
   integer :: ind_in, ind_f  !indices de segmentos de la matriz
   !
   double precision :: aa(nx*ny), bb(nx*ny), cc(nx*ny) ! Variables para almacenar
   !                                          ! matriz tridiagonal sobredimensionada
-  !
 
   !
   ! Variables de postproceso
@@ -66,6 +66,7 @@ Program Calor2D
   cc(:)   = 0.d0
   rr(:)   = 0.d0
   tt(:,:) = 0.d0
+  tr(:,:) = 0.d0
   !
   ! Condiciones de frontera en direcci'on x
   !
@@ -87,22 +88,7 @@ Program Calor2D
   !
   bucle_iteraciones: do iter = 1, itermax
      !
-     ! Inicializamos el valor de la iteraci'on anterior
-    !$omp parallel do
-     do ii = 2, nx-1
-     	do jj= 2. ny-1
-           tr(indicey(ii,jj),1) = tt(indicex(ii,jj),1)
-        end do
-     end do
-     !$omp end parallel do
-     
-     !$omp parallel do
-     do k = 2, (ny-1)*(nx-1)
-           tt(k,2) = tt(k,1)
-           tr(k,2) = tr(k,1)
-     end do
-     !$omp end parallel do
-     !
+     tt_o(:)=tt(:)
      !---------------------------------------------------------------
      !
      ! Paralelizamos el barrido en la direcci'on y en bandas
@@ -111,24 +97,20 @@ Program Calor2D
      !
      !$omp parallel do default(none) &
      !$omp shared( deltax, deltay, tt, tr, cfx, &
-     !$omp aa, bb, cc, rr) &
-     !$omp private(ind_in,ind_f)
+     !$omp aa, bb, cc, rr)
      barrido_y: do jj = 2, ny-1
         !
         ! Es posible combinar directivas de openmp, por ejemplo,
         ! si necesitamos una regi'on paralela unicamente para un bucle,
         ! podemos combinar "parallel" con "do"
-        !
-        ind_in = (jj-1)*nx + 1
-        ind_f  = jj*nx
 
         ensambla_tri_x: do ii = 2, nx-1
 
            aa((jj-1)*nx+ii) = 1.d0/(deltax*deltax)
            bb((jj-1)*nx+ii) =-2.d0*(1.d0/(deltax*deltax)+ 1.d0/(deltay*deltay))
            cc((jj-1)*nx+ii) = 1.d0/(deltax*deltax)
-           rr((jj-1)*nx+ii) =-1.d0/(deltay*deltay)*tt((jj-2)*nx+ii,1)-1.d0/(deltay*deltay)&
-                & * tt((jj)*nx+ii,1)
+           rr((jj-1)*nx+ii) =-1.d0/(deltay*deltay)*tt(indicex(ii,jj-1))-1.d0/(deltay*deltay)&
+                & * tt(indicex(ii,jj+1))
            
         end do ensambla_tri_x
         !
@@ -145,41 +127,58 @@ Program Calor2D
         rr((jj)*nx)    = cfx(jj,2)
         !
         ! Resolver el problema algebraico
-    end barrido_y
+    end do barrido_y
     !$omp end parallel do
-    
+
+
     !$omp parallel do default(none) &
-    !$omp shared( tt, aa, bb, cc, rr ) &
-    inversor_y: do jj=2, ny-1
-    	call tri(aa(ind_in:ind_f),bb(ind_in:ind_f),cc(ind_in:ind_f),rr(ind_in:ind_f),tt(ind_in:ind_f),nx)
+    !$omp shared( tt, aa, bb, cc, rr )
+    inversor_y: do jj = 2, ny-1
+    call tri(aa(indicex(1,jj):indicex(nx,jj)), &
+         bb(indicex(1,jj):indicex(nx,jj)), &
+         cc(indicex(1,jj):indicex(nx,jj)), &
+         rr(indicex(1,jj):indicex(nx,jj)), &
+         tt(indicex(1,jj):indicex(nx,jj)), &
+         nx)
 	
         !
      end do inversor_y
      !$omp end parallel do
      !
+     !TRASNPONER
+     !
+     !
+     !
+     !$omp parallel do collapse(2) default(none) &
+     !$omp shared(tt,tr) &
+     !$omp private(ii,jj)
+      do jj=1,ny
+        do ii=1,nx
+
+          tr(indicey(ii,jj),1) = &
+              tt(indicex(ii,jj),1)
+
+        end do
+     end do
+     !$omp end parallel do
      !---------------------------------------------------------------
      !
      ! Paralelizamos el barrido en la direcci'on x en bandas
      ! compuestas por grupos de l'ineas, observamos que si usamos pocas
      ! l'ineas tenemos una aceleraci'on pobre o ausente
-     ind_in = 0
-     ind_f  = 0
-     !$omp parallel do default(none) &
-     !$omp shared(  deltax, deltay, tt, cfy, &
-     !$omp aa, bb, cc, rr) &
-     !$omp private(ind_in,ind_f)
-     barrido_x: do ii = 2, nx-1
-       ind_in = (ii-1)*ny + 1
-       ind_f  = ii*ny
 
+     !$omp parallel do default(none) &
+     !$omp shared(deltax, deltay, tr, cfy, &
+     !$omp aa, bb, cc, rr)
+     barrido_x: do ii = 2, nx-1
 
         ensambla_tri_y: do jj = 2, ny-1
 
            aa((ii-1)*ny+jj)  = 1.d0/(deltay*deltay)
            bb((ii-1)*ny+jj) =-2.d0*(1.d0/(deltax*deltax)+ 1.d0/(deltay*deltay))
            cc((ii-1)*ny+jj) = 1.d0/(deltay*deltay)
-           rr((ii-1)*ny+jj) =-1.d0/(deltax*deltax)*tt((ii-2)*ny+jj,1)-1.d0/(deltax*deltax)*&
-                tt((ii)*ny+jj,1)
+           rr((ii-1)*ny+jj) =-1.d0/(deltax*deltax)*tr(indicey(ii-1,jj))-1.d0/(deltax*deltax)*&
+                tr(indicey(ii+1,jj))
            
         end do ensambla_tri_y
         !
@@ -197,11 +196,17 @@ Program Calor2D
      end do barrido_x
      !$omp end parallel do
         
-        ! Resolver el problema algebraico
+     ! Resolver el problema algebraico
      !$omp parallel do default(none) &
      !$omp shared( tt, aa, bb, cc, rr, tr)
      inversor_x: do ii = 2, nx-1
-        call tri(aa(ind_in:ind_f),bb(ind_in:ind_f),cc(ind_in:ind_f),rr(ind_in:ind_f),tt(ind_in:ind_f),ny)
+
+         call tri(aa(indicey(ii,1):indicey(ii,ny)), &
+         bb(indicey(ii,1):indicey(ii,ny)), &
+         cc(indicey(ii,1):indicey(ii,ny)), &
+         rr(indicey(ii,1):indicey(ii,ny)), &
+         tr(indicey(ii,1):indicey(ii,ny)), &  ! <-- Cambia tr por tt
+         ny)
         !
         !
         ! Actualizar la temperatura de la placa
@@ -209,7 +214,19 @@ Program Calor2D
 
      end do inversor_x
      !$omp end parallel do
-     !
+     !TRASNPONER
+     !$omp parallel do collapse(2) default(none) &
+     !$omp shared(tt,tr) &
+     !$omp private(ii,jj)
+     do jj=1,ny
+       do ii=1,nx
+
+          tt(indicex(ii,jj),1) = &
+                tr(indicey(ii,jj),1)
+
+       end do
+     end do
+     !$omp end parallel do
      ! Criterio de convergencia
      !
      residuo = 0.d0
@@ -231,16 +248,16 @@ Program Calor2D
      !
   end do bucle_iteraciones
   !
-  write(*,*) "Convergencia en ", iter, " iteraciones"
-  !
-  ! call residuo_temp( tt(1:nx,1:ny,1), deltax, deltay, resid_tt )
+  !write(*,*) "Convergencia en ", iter, " iteraciones"
+  tt_m(:,:) = RESHAPE(tt(:,1), SHAPE=(/nx, ny/))
+  !call residuo_temp( tt(1:nx,1:ny)_m, deltax, deltay, resid_tt )
   !
   ! Aunque es muy tentador, no podemos paralelizar este bucle,
   ! el archivo queda desordenado y gnuplot (y otros graficadores) no
   ! los procesan bien.
   !
-  !archivo = 'salida.vtk'
-  write(*,*),tt
-  ! call postproceso_vtk(xx,yy,tt(1:nx,1:ny,1), resid_tt ,archivo)
+  archivo = 'salida.vtk'
+  write(*,*) 'tt_m = ', tt_m
+  call postproceso_vtk(xx,yy,tt_m(1:nx,1:ny), resid_tt ,archivo)
   !
 end Program Calor2D
